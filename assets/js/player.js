b8c6219e30991f2653fb0ea35617d3c3d23d11ed
Xyrus <91442589+xyrusL@users.noreply.github.com>
2024-12-31 16:51:39 +0800

    Updated project files and configurations.

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,28 +1,36 @@
-let previousButton = $('.playbutton').first().prop('disabled', true);
+let previousButton;
+const lastTime = JSON.parse(localStorage.getItem('lastTime')) || {};
+const animeId = $('#animebtn').data('id');
+
+if (!lastTime[animeId]) {
+    previousButton = $('.playbutton').first().prop('disabled', true);
+} else {
+    previousButton = $(`.playbutton:eq(${lastTime[animeId]})`).prop('disabled', true);
+}
+
 $('.playbutton').on('click', function() {
-    const episodeNumber = $(this).text(); 
-    const animeId = $('#animebtn').data('id');
+    const episodeNumber = $(this).text();
+    const episodeIndex = $(this).index();
     
-    getEpisodes(episodeNumber - 1, animeId); 
-    if (previousButton !== null) {
+    getEpisodes(episodeIndex, animeId);
+    if (previousButton) {
         previousButton.prop('disabled', false);
     }
     $(this).prop('disabled', true);
     previousButton = $(this);
+    saveLastWatched(episodeIndex, animeId);
 });
 
 function getEpisodes(episodeIndex, animeId) {
     $.ajax({
         url: `${baseUrl}home/getEpisodeUrl`,
         method: 'POST',
-        data: {
-            anime_id: animeId,
-            episode_index: episodeIndex
-        },
+        data: { anime_id: animeId, episode_index: episodeIndex },
         success: function(response) {
             const data = JSON.parse(response);
             if (data.url) {
                 $('#iframeplayer').attr('src', data.url);
+                saveLastWatched(episodeIndex, animeId);
             } else {
                 console.error('Episode URL not found');
             }
@@ -32,3 +40,19 @@ function getEpisodes(episodeIndex, animeId) {
         }
     });
 }
+
+function saveLastWatched(index, id) {
+    const lastTime = JSON.parse(localStorage.getItem('lastTime')) || {};
+    lastTime[id] = index;
+    localStorage.setItem('lastTime', JSON.stringify(lastTime));
+}
+
+function handleBackNavigation() {
+    window.addEventListener('popstate', function(event) {
+        window.location.href = '/test';
+    });
+
+    history.pushState(null, '', window.location.href);
+}
+
+handleBackNavigation();
