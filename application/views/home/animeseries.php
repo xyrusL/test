af1557f7a858a7c797c8bd8a8869fc837b91fe03
Xyrus <91442589+xyrusL@users.noreply.github.com>
2025-01-02 09:28:33 +0800

    fix

--- application/views/home/animeseries.php
+++ application/views/home/animeseries.php
@@ -19,31 +19,7 @@
 </head>
 
 <body>
-    <div id="coverlight" style="z-index:8"></div>
-    <div id="loadcontainer" style="display: none;">
-        <div class="loadindicator"></div>
-    </div>
-    
-    <div class="toppart">
-        <h1 style="display:none;">Watch anime online animixplay</h1>
-        <div id="songContent" class="floattopsearch">
-            <a id="backiconhome"><i class="glyphicon glyphicon-chevron-left"></i></a>
-            <a id="homeicon" href="http://localhost/test/"><i class="glyphicon glyphicon-home"></i></a>
-            <div id="searchbox">
-                <input type="text" style="display:none">
-                <input type="password" style="display:none">
-                <a href="http://localhost/test/"><img class="webtitle" alt="AnimixPlay" src="http://localhost/test/assets/logo.png"></a>
-                <input class="form-control searchbar" placeholder="Search" autocomplete="off" id="q" type="search">
-                <button id="searchbutton"><i class="glyphicon glyphicon-search"></i></button>
-                <a class="topmenubtn" title="Random anime" href=""><i class="glyphicon glyphicon-random"></i> Random</a>
-                <a class="topmenubtn" title="A-Z List" href="">A-Z List</a>
-            </div>
-            <span id="usernametop"></span>
-            <button id="menumobilebtn"><i class="glyphicon glyphicon-menu-hamburger"></i></button>
-            <button id="showsearchbtn"><i class="glyphicon glyphicon-search"></i></button>
-        </div>
-    </div>
-
+    <?php include 'navbar.php';?>
     <div class="playersidebar">
         <div style="border: 1px solid #2a2a 2a;">
             <i class="closebtn glyphicon glyphicon-remove" id="menuclose" onclick="showmobilemenu()"></i>
---
+++ application/views/home/navbar.php
@@ -0,0 +1,33 @@
+<div id="coverlight" style="z-index:8"></div>
+<div id="loadcontainer" style="display: none;">
+    <div class="loadindicator"></div>
+</div>
+
+<div class="toppart">
+    <h1 style="display:none;">Watch anime online animixplay</h1>
+    <div id="songContent" class="floattopsearch">
+        <a id="backiconhome"><i class="glyphicon glyphicon-chevron-left"></i></a>
+        <a id="homeicon" href="<?php echo base_url(); ?>"><i class="glyphicon glyphicon-home"></i></a>
+        <div id="searchbox">
+            <input type="text" style="display:none">
+            <input type="password" style="display:none">
+            <a href="<?php echo base_url(); ?>"><img class="webtitle" alt="AnimixPlay" src="<?php echo base_url('assets/logo.png'); ?>"></a>
+            <input class="form-control searchbar" placeholder="Search" autocomplete="off" id="q" type="search">
+            <button id="searchbutton"><i class="glyphicon glyphicon-search"></i></button>
+            <a class="topmenubtn" title="Random anime" href=""><i class="glyphicon glyphicon-random"></i> Random</a>
+            <a class="topmenubtn" title="A-Z List" href="">A-Z List</a>
+        </div>
+        <span id="usernametop"></span>
+        <button id="menumobilebtn"><i class="glyphicon glyphicon-menu-hamburger"></i></button>
+        <button id="showsearchbtn"><i class="glyphicon glyphicon-search"></i></button>
+    </div>
+</div>
+
+<div class="quicksearchcontainer">
+    <div class="resultcontainer">
+        <div id="fullresultbtn">
+            <a>RioSearch</a>
+        </div>
+        <ul class="quickresult"></ul>
+    </div>
+</div>
